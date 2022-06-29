793cea6cac5a982368169cf19154cc7bf8f8faf1
Kannappan Sriramajayam <you@example.com>
2022-06-28 22:42:38 -0400

    AWS EC2 Pricing

---
+++ Pricing.py
@@ -0,0 +1,15 @@
+Instance purchasing options
+
+>On-Demand Instances-Pay, by the second, for the instances that you launch.
+
+>Savings Plans-Reduce costs by making a commitment to a consistent amount of usage, for a term of 1 or 3 years.
+
+>Reserved Instances-Reduce costs by making a commitment to a consistent instance configuration, type and Region, for a term of 1 or 3 years.
+
+>Spot Instances-Request unused EC2 instances, which can reduce your costs significantly.
+
+>Dedicated Hosts-Pay for a physical host that is fully dedicated to running your instances.
+
+>Dedicated Instances – Pay, by the hour, for instances that run on single-tenant hardware.
+
+>Capacity Reservations – Reserve capacity for your instances in a specific Availability Zone for any duration.
